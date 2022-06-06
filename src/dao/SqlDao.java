8749bd024923239b321333aa476b4249979b5c8d
ra54283suzuki <ra54283suzuki@gmail.com>
2022-06-06 21:53:40 +0900

    Implementation of login function

---
+++ src/dao/SqlDao.java
@@ -0,0 +1,67 @@
+package dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import config.DBconfig;
+import dto.LoginUser;
+
+//アクセスロジック(DAO)
+public class SqlDao {
+		//DBconfig.propertiesのフルパス
+		public final String file_path = "/Applications/Eclipse_2020-12.app/Contents/workspace/Java-Login-Eclipse/DBconfig.properties";
+		//DBconfigのオブジェクト生成
+		DBconfig config = new DBconfig();
+
+		//ログイン認証のメソッド
+		public List<LoginUser> check(String user, String password) throws IOException {
+			//DBconfig.propertiesの各値をlist形式で取得
+			String[] DbInfo = config.getDBinfo(file_path);
+
+			//DBconfig.propertiesのurlを取得
+			String url = DbInfo[0];
+			//DBconfig.propertiesのuserを取得
+			String db_user_name = DbInfo[1];
+			//DBconfig.propertiesのpasswordを取得
+			String db_password = DbInfo[2];
+
+			String sql = "select * from login_user_tb "
+					+ "where name = ? and password = ?";
+
+			//ログインユーザのオブジェクト生成(DTO)
+			LoginUser login_user = new LoginUser();
+			//ログインユーザの情報をリスト形式で取得する
+			List<LoginUser> user_info = new ArrayList<LoginUser>();
+
+			//データベースへの接続
+			try(Connection conn = DriverManager.getConnection(url,db_user_name,db_password)){
+				PreparedStatement stmt = conn.prepareStatement(sql);
+				//変数sqlの一番目の?に引数のuserをセットする
+				stmt.setString(1, user);
+				//変数sqlの二番目の?に引数のpasswordをセットする
+				stmt.setString(2, password);
+				//sqlを実行し該当するデータ格納
+				ResultSet rs = stmt.executeQuery();
+
+				if(rs.next()) {
+					login_user.setId(rs.getInt("id"));
+					login_user.setName(rs.getString("name"));
+					login_user.setPassword(rs.getString("password"));
+					user_info.add(login_user);
+				} else {
+					login_user.setName("No user");
+					login_user.setPassword("Not match password");
+					user_info.add(login_user);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			return user_info;
+		}
+}
--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,6 +9,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.SqlDao;
+import dto.LoginUser;
 
 /**
  * Servlet implementation class LoginServlet
@@ -23,7 +27,36 @@ public class LoginServlet extends HttpServlet {
 		dispatcher.forward(request, response);
 	}
 
+	//ログイン認証のメインロジック
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//文字コードの設定
+		response.setContentType("text/html; charset=UTF-8");
+		request.setCharacterEncoding("UTF-8");
+
+		//ログイン画面で入力された値を取得
+		String user = request.getParameter("user_name");
+		String password = request.getParameter("password");
+
+		SqlDao sql = new SqlDao();
+		List<LoginUser> login = sql.check(user,password);
+
+		String login_user = login.get(0).getName();
+		String login_pass = login.get(0).getPassword();
+
+		if(user.equals(login_user) && password.equals(login_pass)) {
+			//ログイン成功→次に画面へ
+			RequestDispatcher dispatcher =
+					request.getRequestDispatcher("WEB-INF/jsp/customer_list.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			//ログイン失敗→ログイン画面へ戻る
+			RequestDispatcher dispatcher =
+					request.getRequestDispatcher("WEB-INF/jsp/login.jsp");
+			dispatcher.forward(request, response);
+		}
+
 
 	}
+
+
 }
